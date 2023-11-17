33e9516c8f10f043347fb7b225b2936f338a6cf0
Gauthier <gauthier.duffier@efrei.net>
2023-11-17 11:08:53 +0100

    Refactor Spotify player component and add track information

--- app/player/page.tsx
+++ app/player/page.tsx
@@ -1,64 +1,142 @@
 "use client";
-import React, { use, useCallback, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
 import styles from "./player.module.css";
 import { BsFillPlayFill } from "react-icons/bs";
 import { CgPlayTrackNext, CgPlayTrackPrev } from "react-icons/cg";
 import { FaRandom } from "react-icons/fa";
 import { TiArrowLoop } from "react-icons/ti";
-import { GiPauseButton } from "react-icons/gi";
-import { WebPlaybackSDK } from "react-spotify-web-playback-sdk";
-import { useState } from "react";
-import SpotifyPlayer from "react-spotify-web-playback";
+import { FaPause } from "react-icons/fa6";
 
-const page = () => {
-  const [token, settoken] = useState("");
-  function getToken() {
-    const token = localStorage.getItem("access_token");
-    if (!token) {
+const spotifyApi = new SpotifyWebApi();
+
+const Page = () => {
+  const [token, setToken] = useState("");
+  const [trackInfo, setTrackInfo] = useState({
+    name: "",
+    artist: "",
+    duration: 0,
+    progress: 0,
+    imageUrl: "",
+  });
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem("access_token");
+
+    if (!storedToken) {
       window.location.href = "http://localhost:3000/";
     } else {
-      settoken(token);
+      setToken(storedToken);
+      spotifyApi.setAccessToken(storedToken);
+      getNowPlaying();
     }
-  }
-  useEffect(() => {
-    getToken();
   }, []);
+
+  const getNowPlaying = async () => {
+    try {
+      const response = await spotifyApi.getMyCurrentPlaybackState();
+      if (!response.item) {
+        return;
+      }
+      const { name, artists, album } = response.item;
+      const duration_ms = response.item.duration_ms || 0;
+      const { progress_ms } = response;
+      const artistNames = artists.map((artist) => artist.name).join(", ");
+      const imageUrl = album.images[0].url;
+      setTrackInfo({
+        name,
+        artist: artistNames,
+        duration: duration_ms,
+        progress: progress_ms || 0,
+        imageUrl,
+      });
+
+      // Schedule the next update
+      setTimeout(getNowPlaying, 1000);
+    } catch (error) {
+      console.error("Error fetching now playing:", error);
+    }
+  };
+
+  const playPauseTrack = async () => {
+    try {
+      const playbackState = await spotifyApi.getMyCurrentPlaybackState();
+      const isPlaying = playbackState.is_playing;
+
+      if (isPlaying) {
+        await spotifyApi.pause();
+      } else {
+        await spotifyApi.play();
+      }
+
+      // Update the now playing information
+      getNowPlaying();
+    } catch (error) {
+      console.error("Error toggling play/pause:", error);
+    }
+  };
+
+  const nextTrack = async () => {
+    try {
+      await spotifyApi.skipToNext();
+
+      // Update the now playing information
+      getNowPlaying();
+    } catch (error) {
+      console.error("Error skipping to the next track:", error);
+    }
+  };
+
+  const previousTrack = async () => {
+    try {
+      await spotifyApi.skipToPrevious();
+
+      // Update the now playing information
+      getNowPlaying();
+    } catch (error) {
+      console.error("Error skipping to the previous track:", error);
+    }
+  };
 
   return (
     <div className={styles.main}>
       <h3>Now Playing</h3>
       <img
         className={styles.mainimage}
-        src="https://cdn.discordapp.com/attachments/887705621426745345/1173646182543790080/image.png?ex=6564b62f&is=6552412f&hm=9408072ce60c69909b0e5faf99e0d406a4f10da6cc13f7c71c8c86a00437551b&"
+        src={trackInfo.imageUrl}
+        alt="Album cover"
       />
-      <h3>Beat it</h3>
-      <p>Michael Jackson</p>
+      <h3>{trackInfo.name}</h3>
+      <p>{trackInfo.artist}</p>
       <div className={styles.slider}>
-        <p>2:15</p>
-        <input type="range" min="0" max="100" />
-        <p>4:18</p>
+        <p>{msToMinutesAndSeconds(trackInfo.progress)}</p>
+        <input
+          type="range"
+          min="0"
+          max={trackInfo.duration}
+          value={trackInfo.progress}
+          readOnly // Make the input readOnly
+        />
+        <p>{msToMinutesAndSeconds(trackInfo.duration)}</p>
       </div>
       <div className={styles.icons}>
-        <TiArrowLoop className={styles.loopicon} />
-        <CgPlayTrackPrev className={styles.previcon} />
-        <div className={styles.card}>
+        <TiArrowLoop className={styles.loopicon} onClick={nextTrack} />
+        <CgPlayTrackPrev className={styles.previcon} onClick={previousTrack} />
+        <div className={styles.card} onClick={playPauseTrack}>
           <BsFillPlayFill className={styles.playicon} />
         </div>
-        <CgPlayTrackNext className={styles.nexticon} />
+        <CgPlayTrackNext className={styles.nexticon} onClick={nextTrack} />
         <FaRandom className={styles.shuffleicon} />
       </div>
-{ 
-  token ? (
-    <SpotifyPlayer
-    token={token}
-    uris={['spotify:artist:6HQYnRM4OzToCYPpVBInuU']}
-  />
-  ) : (
-    <h1>Not Logged In</h1>
-  )  
-  }
     </div>
   );
 };
 
-export default page;
+// Helper function to convert milliseconds to minutes and seconds
+const msToMinutesAndSeconds = (ms: number) => {
+  const minutes = Math.floor(ms / 60000);
+  const seconds = ((ms % 60000) / 1000).toFixed(0);
+  return `${minutes}:${Number(seconds) < 10 ? "0" : ""}${seconds}`;
+};
+
+export default Page;
